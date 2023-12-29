4a02e6e7558a7fcadb0682272f7aadd6155df2f3
Bauyrzhan Yerdaliyev <baurzhanerdaliev@gmail.com>
2023-12-28 21:22:14 -0500

    quadratic equation solver

---
+++ quadratic_equation.py
@@ -0,0 +1,17 @@
+class Quadratic:
+    def __init__(self):
+        self.a = float(input("Enter a: "))
+        self.b = float(input("Enter b: "))
+        self.c = float(input("Enter c: "))
+    
+
+    def roots(self):
+        D = (self.b * self.b - 4 * self.a * self.c) ** 0.5
+        x_1 = (-self.b + D) / (2 * self.a)
+        x_2 = (-self.b - D) / (2 * self.a)
+        print(f"{x_1=}, {x_2=}")
+
+
+if __name__ == "__main__":
+    run = Quadratic()
+    run.roots()
